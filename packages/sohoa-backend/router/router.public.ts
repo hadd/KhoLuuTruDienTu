e2e922e8abc6788ae76b83121215cbfbdbc1864d
longdthe172082 <longdthe172082@fpt.edu.vn>
2026-08-19 16:52:27 +0700

    man hinh quet

--- packages/sohoa-backend/router/router.public.ts
+++ packages/sohoa-backend/router/router.public.ts
@@ -4,9 +4,9 @@ import { env } from "../env.ts";
 
 export const PUBLIC_PREFIX = "/api/public";
 export const publicRouter = new Elysia({
-    prefix: PUBLIC_PREFIX,
+  prefix: PUBLIC_PREFIX,
 });
 
 publicRouter.use(healthPlugin({
-    db_connection_str: env.DATABASE_URL,
+  db_connection_str: env.DATABASE_URL,
 }));
